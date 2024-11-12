bb3414f11094fd06070eeafc9629d0d46bf4e458
kevin <7480060+kevinLuan@user.noreply.gitee.com>
2024-11-12 14:18:34 +0800

    When the MOCK object, skip the final and static properties to initialize the MOCK data

--- src/main/java/cn/taskflow/jcv/codegen/MockDataGenerator.java
+++ src/main/java/cn/taskflow/jcv/codegen/MockDataGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.taskflow.jcv.codegen;
 
 import cn.taskflow.jcv.encode.GsonEncoder;
@@ -61,10 +77,15 @@ public class MockDataGenerator {
         visitedClasses.add(clazz);
         try {
             Object instance = createInstance(clazz, visitedClasses);
-            for (Field field : clazz.getDeclaredFields()) {
-                field.setAccessible(true);
-                Object value = generateMockValue(field.getType(), field.getGenericType(), visitedClasses);
-                field.set(instance, value);
+            if (!clazz.getName().startsWith("java.")) {
+                for (Field field : clazz.getDeclaredFields()) {
+                    if (Modifier.isStatic(field.getModifiers()) || Modifier.isFinal(field.getModifiers())) {
+                        continue;//skip
+                    }
+                    field.setAccessible(true);
+                    Object value = generateMockValue(field.getType(), field.getGenericType(), visitedClasses);
+                    field.set(instance, value);
+                }
             }
             return instance;
         } finally {
--- src/test/java/cn/taskflow/jcv/codegen/Person.java
+++ src/test/java/cn/taskflow/jcv/codegen/Person.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.taskflow.jcv.codegen;
 
 import lombok.Data;
@@ -7,6 +23,8 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author SHOUSHEN.LUAN
@@ -14,24 +32,28 @@ import java.util.Map;
  */
 @Data
 public class Person {
-    private String name;
-    private int age;
-    private String email;
-    private List<Person> friends; // Example of a nested collection
-    private Map<String, String> attributes; // Example of a map
-    private Address address;
-    private Order order;
-    private User user;
+    final Byte                  aByte  = 0;
+    static Short                aShort = 1;
+    AtomicLong                  num;
+    private AtomicBoolean       atomicBoolean;
+    private String              name;
+    private int                 age;
+    private String              email;
+    private List<Person>        friends;      // Example of a nested collection
+    private Map<String, String> attributes;   // Example of a map
+    private Address             address;
+    private Order               order;
+    private User                user;
 
     @Data
     public static class Order {
-        private String orderId;
-        private String product;
-        private int quantity;
-        private double price;
-        private Date orderDate;
+        private String      orderId;
+        private String      product;
+        private int         quantity;
+        private double      price;
+        private Date        orderDate;
         private List<Goods> goods;
-        private Timestamp createTime;
+        private Timestamp   createTime;
     }
 
     @Data
@@ -51,14 +73,15 @@ public class Person {
         private String name;
         private String category;
         private double price;
-        private int quantity;
+        private int    quantity;
     }
+
     @RequiredArgsConstructor
     @Data
     public static class User {
         private final String username;
         private final String password;
         private final String email;
-        private final Date registrationDate;
+        private final Date   registrationDate;
     }
 }
